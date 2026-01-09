1c4a0cd0a14ada069ab971558af655be48278067
Alex Viaselyeu <aliaksei.viaselyeu@gmail.com>
2026-01-09 13:37:12 +0100

    Almost ready after friday session with Aleksandr

--- project/project.cpp
+++ project/project.cpp
@@ -5,67 +5,31 @@
 #include <filesystem>
 #include <fstream>
 #include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <boost/interprocess/sync/interprocess_mutex.hpp>
+#include <boost/interprocess/sync/interprocess_condition.hpp>
 
 namespace po = boost::program_options;
 namespace ip = boost::interprocess;
+
+ip::interprocess_mutex mtx;
+ip::interprocess_condition cv;
 
 constexpr unsigned int max_memory_size = 1048576; // 1 MB
 constexpr unsigned int chunk_size = 65535;
 bool memory_not_needed = false;
 
-bool memory_exists(const std::string& name) {
-    try {
-        ip::shared_memory_object shm(
-            ip::open_only,
-            name.c_str(),
-            ip::read_write
-        );
-        return true; // Memory object exists
-    }
-    catch (const ip::interprocess_exception& ex) {
-        return false; // Memory object does not exist
-    }
-}
+constexpr unsigned int CREATOR = 0;
+constexpr unsigned int USER = 1;
+constexpr unsigned int MEM_SIZE = 2 * 65535;
 
-class MemoryHolder{
-public:
-    ip::shared_memory_object shm;
-	std::string memory_name;
-	bool remove_needed = true;
-
-    MemoryHolder(const std::string& name, std::size_t size, bool create)
-    {	
-		memory_name.append(name);
-        remove_needed = create;
-        if(create){
-            shm = ip::shared_memory_object(
-                ip::create_only,
-                name.c_str(),
-                ip::read_write
-            );
-            shm.truncate(size);
-            std::cout << "Memory object created. " << std::endl;
-        }else{
-            shm = ip::shared_memory_object(
-                ip::open_only,
-                name.c_str(),
-                ip::read_write
-            );
-            std::cout << "Memory object opened. " << std::endl;
-        }
-    }
-
-    ~MemoryHolder()
-    {
-        if(remove_needed){
-            ip::shared_memory_object::remove(memory_name.c_str());
-            std::cout << "Memory object removed. " << std::endl;
-        }
-        else {
-			std::cout << "Memory object released. " << std::endl;
-        }
-    }
-};
+unsigned char buf_prod = 0;  // number of buffer for the next operation of producer
+unsigned char buf_cons = 0;  // number of buffer for the next operation of consumer
+unsigned char prod_num = 2;  // number of available for procurer buffers
+unsigned char cons_num = 0;  // number of available for consumer buffers
+int actual_size;
+bool creator_finish = false;
+bool user_finish = false;
 
 int main(int argc, char* argv[])
 {
@@ -101,50 +65,77 @@ int main(int argc, char* argv[])
 		std::cout << "Memory: " << vm["memory"].as<std::string>() << std::endl;*/
 
         // ============== MAIN LOGIC IS HERE ======================================
+        std::unique_ptr<ip::shared_memory_object> shm_obj_ptr;
 
-        constexpr unsigned int CREATOR = 0;
-        constexpr unsigned int USER = 1;
-		int role = CREATOR; // 0 - creator, 1 - user
+        int role;
 
-        if( memory_exists(vm["memory"].as<std::string>())){
+	    const auto & mem_name = vm["memory"].as<std::string>();
+
+        // memory is created or opened if already exists
+        try {
+            shm_obj_ptr = std::make_unique<ip::shared_memory_object>(ip::create_only, mem_name.c_str(), ip::read_write);
+            shm_obj_ptr->truncate(sizeof(int) * MEM_SIZE);
+            role = CREATOR;
+        }
+        catch (const ip::interprocess_exception& ex) {
+            shm_obj_ptr = std::make_unique<ip::shared_memory_object>(ip::open_only, mem_name.c_str(), ip::read_write);
             role = USER;
-		}
+        }
 
-        if(role == USER){
+        ip::mapped_region region(*shm_obj_ptr, ip::read_write);
+        char* shm_array = static_cast<char*>(region.get_address());
+        char (*buf)[2] = reinterpret_cast<char (*)[2]>(shm_array);
+
+        if(role == CREATOR){
 
             std::ifstream input_file(vm["source"].as<std::string>(), std::ios::binary);
-            if (!input_file) {
-                std::cerr << "Error opening source file: " << vm["source"].as<std::string>() << std::endl;
-                return 1;
-            }else{ // here the work with file
-                MemoryHolder memory_holder(
-                    vm["memory"].as<std::string>(),
-                    max_memory_size,
-                    false
-				);
+            
+            while (!input_file.eof()) {
+                {
+                    ip::scoped_lock<boost::interprocess::interprocess_mutex> lock(mtx);
+                    while (prod_num <= 0) {
+                        cv.wait(lock);
+                    }
+                    prod_num--;
+                }
 
+                input_file.read(buf[buf_prod], chunk_size);
+                buf_prod ^= 0x01;
 
-
-				memory_not_needed = true;
+                {
+                    ip::scoped_lock<boost::interprocess::interprocess_mutex> lock(mtx);
+                    actual_size = input_file.gcount();  // transmit size to consumer
+                    cons_num++;
+                    cv.notify_one();
+                }
             }
-		}else {  // role == CREATOR
+            creator_finish = true;
 
-            std::ofstream input_file(vm["destination"].as<std::string>(), std::ios::binary);
-            if (!input_file) {
-                std::cerr << "Error opening source file: " << vm["destination"].as<std::string>() << std::endl;
-                return 1;
-            }else { // here the work with file
-                MemoryHolder memory_holder(
-                    vm["memory"].as<std::string>(),
-                    max_memory_size,
-                    true
-                );
+            while(!user_finish){
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			}
 
-
-
-
-				do {} while (!memory_not_needed);
+            ip::shared_memory_object::remove(vm["memory"].as<std::string>().c_str());
+        }
+        else {
+            std::ofstream output_file(vm["destination"].as<std::string>(), std::ios::binary);
+            
+            while (true) {
+                {
+                    ip::scoped_lock<boost::interprocess::interprocess_mutex> lock(mtx);
+                    cv.wait(lock, [] { return cons_num > 0; });
+                    cons_num--;
+                }
+                output_file.write(buf[buf_cons], actual_size);
+                buf_cons ^= 0x01;
+                {
+                    ip::scoped_lock<boost::interprocess::interprocess_mutex> lock(mtx);
+                    prod_num++;
+                    if (creator_finish && (cons_num == 0)) break;
+                    cv.notify_one();
+                }
             }
+			user_finish = true;
         }
     }
 
