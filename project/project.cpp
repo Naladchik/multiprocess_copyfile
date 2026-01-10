3c1bfb9fec19344ebd96dc617fdb7c2ecb75cbb7
Alex Viaselyeu <aliaksei.viaselyeu@gmail.com>
2026-01-10 21:30:36 +0100

    Possble race condition. sch_vars = new (region.get_address()) expression needs a pause before to work properly

--- project/project.cpp
+++ project/project.cpp
@@ -12,24 +12,28 @@
 namespace po = boost::program_options;
 namespace ip = boost::interprocess;
 
-ip::interprocess_mutex mtx;
-ip::interprocess_condition cv;
-
-constexpr unsigned int max_memory_size = 1048576; // 1 MB
 constexpr unsigned int chunk_size = 65535;
-bool memory_not_needed = false;
+constexpr unsigned int mem_size = 3 * chunk_size;
 
 constexpr unsigned int CREATOR = 0;
 constexpr unsigned int USER = 1;
-constexpr unsigned int MEM_SIZE = 2 * 65535;
 
-unsigned char buf_prod = 0;  // number of buffer for the next operation of producer
-unsigned char buf_cons = 0;  // number of buffer for the next operation of consumer
-unsigned char prod_num = 2;  // number of available for procurer buffers
-unsigned char cons_num = 0;  // number of available for consumer buffers
-int actual_size;
-bool creator_finish = false;
-bool user_finish = false;
+class SharedVars {
+public:
+    ip::interprocess_mutex mtx;
+    ip::interprocess_condition cv;
+
+    char buf[2][chunk_size] = {0};
+
+    unsigned char buf_prod = 0;  // number of buffer for the next operation of producer
+    unsigned char buf_cons = 0;  // number of buffer for the next operation of consumer
+    unsigned char prod_num = 2;  // number of available for procurer buffers
+    unsigned char cons_num = 0;  // number of available for consumer buffers
+    int actual_size = 0;
+    bool creator_finish = false;
+	bool user_finish = false;
+};
+
 
 int main(int argc, char* argv[])
 {
@@ -74,68 +78,80 @@ int main(int argc, char* argv[])
         // memory is created or opened if already exists
         try {
             shm_obj_ptr = std::make_unique<ip::shared_memory_object>(ip::create_only, mem_name.c_str(), ip::read_write);
-            shm_obj_ptr->truncate(sizeof(int) * MEM_SIZE);
+            shm_obj_ptr->truncate(sizeof(int) * mem_size);
             role = CREATOR;
+            std::cout << "CREATOR: STARTED" << std::endl;
         }
-        catch (const ip::interprocess_exception& ex) {
+        catch (const ip::interprocess_exception&) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
             shm_obj_ptr = std::make_unique<ip::shared_memory_object>(ip::open_only, mem_name.c_str(), ip::read_write);
             role = USER;
+            std::cout << "USER: STARTED" << std::endl;
         }
 
         ip::mapped_region region(*shm_obj_ptr, ip::read_write);
-        char* shm_array = static_cast<char*>(region.get_address());
-        char (*buf)[2] = reinterpret_cast<char (*)[2]>(shm_array);
+        if (role == USER) { std::cout << "LABEL: 1" << std::endl; }
+        try {
+            SharedVars* sch_vars = new (region.get_address()) SharedVars;
+        }
+        catch (const ip::interprocess_exception& ex) {
+            std::cout << "EXCEPTION: " << ex.what() << std::endl;
+        }
+        if (role == USER) { std::cout << "LABEL: 2" << std::endl; }
 
         if(role == CREATOR){
+            std::cout << "CREATOR: STARTED INSIDE" << std::endl;
 
-            std::ifstream input_file(vm["source"].as<std::string>(), std::ios::binary);
-            
-            while (!input_file.eof()) {
-                {
-                    ip::scoped_lock<boost::interprocess::interprocess_mutex> lock(mtx);
-                    while (prod_num <= 0) {
-                        cv.wait(lock);
-                    }
-                    prod_num--;
-                }
+   //         std::ifstream input_file(vm["source"].as<std::string>(), std::ios::binary);
+   //         
+   //         while (!input_file.eof()) {
+   //             {
+   //                 ip::scoped_lock<ip::interprocess_mutex> lock(sch_vars->mtx);
+   //                 sch_vars->cv.wait(lock, [sch_vars] { return sch_vars->prod_num > 0; });
+   //                 sch_vars->prod_num--;
+   //             }
 
-                input_file.read(buf[buf_prod], chunk_size);
-                buf_prod ^= 0x01;
+   //             input_file.read(sch_vars->buf[sch_vars->buf_prod], chunk_size);
+   //             sch_vars->buf_prod ^= 0x01;
 
-                {
-                    ip::scoped_lock<boost::interprocess::interprocess_mutex> lock(mtx);
-                    actual_size = input_file.gcount();  // transmit size to consumer
-                    cons_num++;
-                    cv.notify_one();
-                }
-            }
-            creator_finish = true;
+   //             {
+   //                 ip::scoped_lock<boost::interprocess::interprocess_mutex> lock(sch_vars->mtx);
+   //                 sch_vars->actual_size = (unsigned int) input_file.gcount();  // transmit size to consumer
+   //                 sch_vars->cons_num++;
+   //                 sch_vars->cv.notify_one();
+   //             }
+   //         }
+   //         sch_vars->creator_finish = true;
 
-            while(!user_finish){
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
-			}
+            //while(!sch_vars->user_finish){
+                std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+			//}
 
             ip::shared_memory_object::remove(vm["memory"].as<std::string>().c_str());
-        }
-        else {
-            std::ofstream output_file(vm["destination"].as<std::string>(), std::ios::binary);
-            
-            while (true) {
+            std::cout << "CREATOR: FINISHED" << std::endl;
+
+		}
+		else { // role == USER
+            std::cout << "USER: STARTED INSIDE" << std::endl;
+            //std::ofstream output_file(vm["destination"].as<std::string>(), std::ios::binary);
+
+            /*while (true) {
                 {
-                    ip::scoped_lock<boost::interprocess::interprocess_mutex> lock(mtx);
-                    cv.wait(lock, [] { return cons_num > 0; });
-                    cons_num--;
+                    ip::scoped_lock<boost::interprocess::interprocess_mutex> lock(sch_vars->mtx);
+                    sch_vars->cv.wait(lock, [sch_vars] { return sch_vars->cons_num > 0; });
+                    sch_vars->cons_num--;
                 }
-                output_file.write(buf[buf_cons], actual_size);
-                buf_cons ^= 0x01;
+                output_file.write(sch_vars->buf[sch_vars->buf_cons], sch_vars->actual_size);
+                sch_vars->buf_cons ^= 0x01;
                 {
-                    ip::scoped_lock<boost::interprocess::interprocess_mutex> lock(mtx);
-                    prod_num++;
-                    if (creator_finish && (cons_num == 0)) break;
-                    cv.notify_one();
+                    ip::scoped_lock<boost::interprocess::interprocess_mutex> lock(sch_vars->mtx);
+                    sch_vars->prod_num++;
+                    if (sch_vars->creator_finish && (sch_vars->cons_num == 0)) break;
+                    sch_vars->cv.notify_one();
                 }
-            }
-			user_finish = true;
+            }*/
+            //sch_vars->user_finish = true;
+            std::cout << "USER: FINISHED" << std::endl;
         }
     }
 
