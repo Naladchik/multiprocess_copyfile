6fc8f97ebdc82449f1463a60fe1105032aa4969d
Alex Viaselyeu <aliaksei.viaselyeu@gmail.com>
2026-01-25 11:26:34 +0100

    More complicated mix of instances requested

--- project/start.bat
+++ project/start.bat
@@ -1,7 +1,11 @@
 @echo off
 ::start /B build/WorkProject.exe --help
+start /B build/WorkProject.exe --source input1_initial.txt --destination output4.txt --memory mem_shared
 start /B build/WorkProject.exe --source input1.txt --destination output1.txt --memory mem_shared
 start /B build/WorkProject.exe --source input1.txt --destination output2.txt --memory mem_shared
 start /B build/WorkProject.exe --source input1.txt --destination output1.txt --memory mem_shared
+start /B build/WorkProject.exe --source input1_initial.txt --destination output4.txt --memory mem_shared
 start /B build/WorkProject.exe --source input1.txt --destination output2.txt --memory mem_shared
+start /B build/WorkProject.exe --source input1.txt --destination output2.txt --memory mem_shared
+start /B build/WorkProject.exe --source input1.txt --destination output3.txt --memory mem_shared
 pause
