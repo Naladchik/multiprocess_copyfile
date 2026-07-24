760295c4a9c7081177e2152911095661c65d85c4
Alex Viaselyeu <aliaksei.viaselyeu@gmail.com>
2026-07-24 13:58:03 +0200

    Zero-size problem solved

--- project/project.cpp
+++ project/project.cpp
@@ -210,7 +210,7 @@ static void run_server()
 
     bytes_read = boost::asio::read(socket, boost::asio::buffer(buffer_pdu, size_of_read), error);
 
-    std::cout << "[Server] PDU received with size " << bytes_read << " bytes. It is file info." << std::endl;
+    //std::cout << "[Server] PDU received with size " << bytes_read << " bytes. It is file info." << std::endl;
 
     if (error)
     {
@@ -241,76 +241,82 @@ static void run_server()
 
     while (true)
     {
-        if ((file_size - total_bytes_received) < kChunkSize) {
-            size_of_read = file_size - total_bytes_received + kHeaderSize;
-        }
+        if (file_size > 0) {
+            if ((file_size - total_bytes_received) < kChunkSize) {
+                size_of_read = file_size - total_bytes_received + kHeaderSize;
+            }
 
-        bytes_read = boost::asio::read(socket, boost::asio::buffer(buffer_pdu, size_of_read), error);
+            bytes_read = boost::asio::read(socket, boost::asio::buffer(buffer_pdu, size_of_read), error);
 
-        if (error == boost::asio::error::eof)
-        {
-            std::cout << "[Server] Status: DISCONNECTED (client closed the connection)" << endl;
-            if (!file_received) {
-                std::cout << "[Server] Status: ERROR – File transfer incomplete. Received " << total_bytes_received << " bytes out of " << file_size << " bytes." << endl;
+            if (error == boost::asio::error::eof)
+            {
+                std::cout << "[Server] Status: DISCONNECTED (client closed the connection)" << endl;
+                if (!file_received) {
+                    std::cout << "[Server] Status: ERROR – File transfer incomplete. Received " << total_bytes_received << " bytes out of " << file_size << " bytes." << endl;
+                    output_file.close();
+                    fs::path file_path = "received/" + file_name;
+                    try {
+                        if (fs::remove(file_path)) {
+                            std::cout << "[Server] File successfully deleted.\n";
+                        }
+                        else {
+                            std::cout << "[Server] File did not exist.\n";
+                        }
+                    }
+                    catch (const fs::filesystem_error& err) {
+                        std::cerr << "[Server] Filesystem error: " << err.what() << '\n';
+                    }
+                }
+                break;
+            }
+
+            if (error)
+            {
+                std::cout << "[Server] Status: ERROR – " << error.message() << endl;
+                break;
+            }
+
+            //std::cout << "[Server] PDU received with size " << bytes_read << "bytes. Payload is " << bytes_read - kHeaderSize << " bytes." << std::endl;
+
+            // DECRYPTION HERE
+            bytes_read -= kHeaderSize;
+            try {
+                // Pass the actual payload size (bytes_read) to decrypt_chunk
+                decrypt_chunk(buffer_pdu, bytes_read, buffer_payload, secret_key);
+            }
+            catch (const std::exception& e) {
+                std::cerr << "Cryptographic Error: " << e.what() << std::endl;
+                // SECURE CLEANUP: If integrity check fails, stop writing and delete the corrupted file
                 output_file.close();
                 fs::path file_path = "received/" + file_name;
                 try {
-                    if (fs::remove(file_path)) {
-                        std::cout << "[Server] File successfully deleted.\n";
-                    }
-                    else {
-                        std::cout << "[Server] File did not exist.\n";
-                    }
+                    fs::remove(file_path);
                 }
-                catch (const fs::filesystem_error& err) {
-                    std::cerr << "[Server] Filesystem error: " << err.what() << '\n';
+                catch (...) {}
+                return;
+            }
+
+            if (bytes_read >= 0) {
+                output_file.write(buffer_payload.data(), bytes_read);
+                total_bytes_received += bytes_read;
+                if (total_bytes_received == file_size) {
+                    file_received = true;
+                    size_of_read = 0;
+                    std::cout << "[Server] Received the whole file " << endl;
+                    break;
                 }
+
+                if (total_bytes_received > file_size) {
+                    std::cout << "[Server] Received more data than expected! " << total_bytes_received << endl;
+                    break;
+                }
+
+                if (file_size - total_bytes_received < kChunkSize) size_of_read = file_size - total_bytes_received;
             }
+        }
+        else {
+            std::cout << "[Server] Received the whole file " << endl;
             break;
-        }
-
-        if (error)
-        {
-            std::cout << "[Server] Status: ERROR – " << error.message() << endl;
-            break;
-        }
-
-        std::cout << "[Server] PDU received with size " << bytes_read << "bytes. Payload is " << bytes_read - kHeaderSize << " bytes." << std::endl;
-
-        // DECRYPTION HERE
-        bytes_read -= kHeaderSize;
-        try {
-            // Pass the actual payload size (bytes_read) to decrypt_chunk
-            decrypt_chunk(buffer_pdu, bytes_read, buffer_payload, secret_key);
-        }
-        catch (const std::exception& e) {
-            std::cerr << "Cryptographic Error: " << e.what() << std::endl;
-            // SECURE CLEANUP: If integrity check fails, stop writing and delete the corrupted file
-            output_file.close();
-            fs::path file_path = "received/" + file_name;
-            try {
-                fs::remove(file_path);
-            }
-            catch (...) {}
-            return;
-        }
-
-        if (bytes_read > 0) {
-            output_file.write(buffer_payload.data(), bytes_read);
-            total_bytes_received += bytes_read;
-            if (total_bytes_received == file_size) {
-                file_received = true;
-                size_of_read = 0;
-                std::cout << "[Server] Received the whole file " << endl;
-                break;
-            }
-
-            if (total_bytes_received > file_size) {
-                std::cout << "[Server] Received more data than expected! " << total_bytes_received << endl;
-                break;
-            }
-
-            if (file_size - total_bytes_received < kChunkSize) size_of_read = file_size - total_bytes_received;
         }
     }
 }
@@ -396,7 +402,7 @@ static void run_client()
             std::cerr << "Cryptographic Error: " << e.what() << std::endl;
         }
 
-        std::cout << "[Client] transmitting PDU " << bytes_read + kHeaderSize << " bytes with payload " << bytes_read << " bytes." << std::endl;
+        //std::cout << "[Client] transmitting PDU " << bytes_read + kHeaderSize << " bytes with payload " << bytes_read << " bytes." << std::endl;
 
         boost::asio::write(socket, boost::asio::buffer(buffer_pdu, bytes_read + kHeaderSize), error);   // <-- blocks here
 
